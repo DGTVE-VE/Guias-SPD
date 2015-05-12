2eb665cabeefd90a617f164fbe8f41603c0eae21
Sonia Martinez <soniamartinezctr@gmail.com>
2015-05-12 13:01:12 -0500

    cambió el front por el template del servicio profesional docente

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ $modulos = [
     "downloadRecurso" => "downloadRecurso.php",
     "downloadGuia" => "downloadGuia.php",
     "mensaje" => "mensaje.php",
-    "uploadMateriales" => "uploadMaterialForm.php"
+    "uploadMateriales" => "uploadMaterialForm.php",
+    "repositorio" => "repositorio.php"
 ];
 
 //Modulo por default
@@ -31,27 +32,40 @@ if (array_key_exists($module, $modulos)) {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <link href="css/estilo.css" type="text/css" rel="stylesheet">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>        
         <script src="js/spd.js"></script>
         <title>Guías Servicio Profesional Docente</title>
     </head>
     <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">                    
-                    <a class="navbar-brand" href="#">GSPD</a>
-                    
-                </div>
-                <div id="navbar" >
-                    <ul class="nav navbar-nav">
-                        <li><a href="?m=showGuias">Guías</a></li>
-                        <li><a href="?m=uploadGuia">Subir</a></li>
-                    </ul>
-                </div><!--/.nav-collapse -->
-                <div class="navbar-collapse collapse">
-                    
-                <?php if (!isset($_SESSION['usuario'])){?>                
+        <div class="col-md-1"></div>
+        <div class="col-md-10">           
+        <div><img src="imgs/logo.png" width="70%" class="center-block"></div>
+        <nav role="navigation" class="navbar navbar-default">
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                    <span class="sr-only">Menú</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+<!--                <a href="#" class="navbar-brand">Logotipo</a>-->
+            </div>
+
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="?m=showGuias">Guias <b class="caret"></b></a>
+                            <ul role="menu" class="dropdown-menu">
+                                <li><a href="#">Básica</a></li>
+                                <li><a href="#">Media Superior</a></li>
+                            </ul>
+                    </li>                    
+                        <li><a href="?m=perfiles">Perfiles</a></li>
+                        <li><a href="?m=repositorio">Repositorio</a></li>
+                </ul>
+               <?php if (!isset($_SESSION['usuario'])){?>                
                     <form method="POST" action="login.php" class="navbar-form navbar-right">
                         <div class="form-group">
                             <input type="text" name="email" placeholder="Email" class="form-control">
@@ -67,41 +81,85 @@ if (array_key_exists($module, $modulos)) {
                         <li><a href="cierraSesion.php">Cerrar sesión</a></li>
                     </ul>
                 <?php } ?>
-                </div><!--/.navbar-collapse -->
-                
             </div>
         </nav>
+        
+<!--        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">                    
+                    <a class="navbar-brand" href="#">GSPD</a>
+                    
+                </div>
+                <div id="navbar" >
+                    <ul class="nav navbar-nav">
+                        <li><a href="?m=showGuias">Guías</a></li>
+                        <li><a href="?m=uploadGuia">Subir</a></li>
+                    </ul>
+                </div>/.nav-collapse 
+                <div class="navbar-collapse collapse">
+                    
+                <?php// if (!isset($_SESSION['usuario'])){?>                
+                    <form method="POST" action="login.php" class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="text" name="email" placeholder="Email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" placeholder="Password" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-success">Inicia sesión</button>                            
+                    </form>
+                <?php// } ?>
+                <?php// if (isset($_SESSION['usuario'])){?>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="cierraSesion.php">Cerrar sesión</a></li>
+                    </ul>
+                <?php// } ?>
+                </div>/.navbar-collapse 
+                
+            </div>
+        </nav>-->
         <div class="container-fluid theme-showcase" role="main">
             <!-- Main jumbotron for a primary marketing message or call to action -->
-            <div class="jumbotron">
+<!--            <div class="jumbotron">
                 <h1>Servicio profesional docente</h1>
                 <p>Descarga la guía y los recursos asociados a esta para estudiar</p>
-            </div>
+            </div>-->
             <div class="row"> 
-                <div class="col-md-2 sidebar">
+<!--                <div class="col-md-2 sidebar">
                     <ul class="nav nav-sidebar">
                         <li> <a href="?m=showGuias">Guias</a></li>                        
                         <li> <a href="#">Perfiles </a></li>
                         <li> <a href="#">Repositorio </a></li>                        
                     </ul>
-                    <?php if (isset ($_SESSION['usuario'])){?>
+                    <?php// if (isset ($_SESSION['usuario'])){?>
                     <h3>Administración</h3>
                     <ul class="nav nav-sidebar">
                         <li> <a href="?m=uploadGuia">Sube Guias</a></li>
                         <li> <a href="?m=uploadMateriales">Sube Materiales</a></li>                        
                     </ul>
-                    <?php }?>
-                </div>
-                <div class="col-md-10">
+                    <?php// }?>
+                </div>-->
+                <div class="col-md-12">
                     <?php require $modulo; ?>
                 </div>
             </div>
-            
-            <div class="row"> 
-                <div class="col-md-12">
-                    &copy; DGTVE 2015
-                </div>
-            </div>
         </div>
+        </div>
+        <div class="col-md-2"></div>
+                    <footer class="footer col-md-12">
+                <div class="container">
+                    <p class="text-muted text-center">© Ventana Educativa</p>
+                    <div class="col-md-12"> 
+                    <img class="col-md-1 logofoot2" src="imgs/sep.png">
+                    <img class="col-md-1 logofoot" src="imgs/seb.jpg">
+                    <img class="col-md-1 logofoot" src="imgs/imgres.jpg">
+                    <img class="col-md-1 logofoot" src="imgs/inee.png">
+                    <img class="col-md-1 logofoot1" src="imgs/logo-dgtve.jpg">
+                    <img class="col-md-1 logofoot" src="imgs/log_ilce.png">
+                    <img class="col-md-1 logofoot" src="imgs/logounadm.png">
+                    <img class="col-md-1 logofoot" src="imgs/normateca.png">
+                    </div>
+                </div>
+            </footer>
     </body>
 </html>
---
+++ repositorio.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="col-md-5">
+            <h3>Ingresa filtro:</h3>
+            <form role="search">
+                <div class="input-group">
+                    <input type="text" class="form-control"  placeholder="Buscar">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-7"></div>
+        <div class="col-md-12">
+            
+        </div>
+    </div>
+</div>
+<?php

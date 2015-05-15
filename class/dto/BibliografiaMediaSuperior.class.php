911d82728606aa19a06b898de1b108c2a65dd00f
Israel Toledo <j.israel.toledo@gmail.com>
2015-05-15 14:34:26 -0500

    No se usan las tablas

--- class/dto/BibliografiaMediaSuperior.class.php
+++
@@ -1,30 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'bibliografia_media_superior'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2015-05-13 23:51	 
-	 */
-	class BibliografiaMediaSuperior{
-		
-		var $nUMERO;
-		var $pERFIL;
-		var $gUIADEESTUDIO;
-		var $fUNCION;
-		var $pROCESO;
-		var $nIVELSERVICIO;
-		var $dIMENSION;
-		var $bIBLIOGRAFIA;
-		var $dISPONIBLEENLINEA;
-		var $oRIGEN;
-		var $eDITORIAL;
-		var $lIGAOREFERENCIA;
-		var $nOMENCLATURA;
-		var $mATERIALDEDESCARGA;
-		var $oBSERVACIONES;
-		var $uRLPERFIL;
-		var $uRLGUIA;
-		var $uRLMATERIAL;
-		
-	}
-?>
--- class/dto/Perfile.class.php
+++
@@ -1,18 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'perfiles'
-	 *
-     	 * @author: http://phpdao.com
-     	 * @date: 2015-05-12 19:12	 
-	 */
-	class Perfile{
-		
-		var $nUMERO;
-		var $pPI;
-		var $pROCESO;
-		var $fUNCION;
-		var $nIVELMODALIDAD;
-		var $uRL;
-		
-	}
-?>

d21ab04867f5c647464cd4d0fc3475a7428d0507
Israel Toledo <j.israel.toledo@gmail.com>
2015-05-19 15:38:55 -0500

    Script para cambiar los nombres de los archivos

---
+++ changeNames.php
@@ -0,0 +1,85 @@
+<?php
+
+$archivos = scandir ( 'guias' );
+function sanear_string($string)
+{
+
+    $string = trim($string);
+
+    $string = str_replace(
+        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+        $string
+    );
+
+    $string = str_replace(
+        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+        $string
+    );
+
+    $string = str_replace(
+        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ñ', 'Ñ', 'ç', 'Ç'),
+        array('n', 'N', 'c', 'C',),
+        $string
+    );
+
+    //Esta parte se encarga de eliminar cualquier caracter extraño
+    $string = str_replace(
+        array("\\", "¨", "º", "-", "~",
+             "#", "@", "|", "!", "\"",
+             "·", "$", "%", "&", "/",
+             "(", ")", "?", "'", "¡",
+             "¿", "[", "^", "`", "]",
+             "+", "}", "{", "¨", "´",
+             ">", "< ", ";", ",", ":",
+             "."),
+        '',
+        $string
+    );
+    
+    $string = str_replace(
+        array(" "),
+        '_',
+        $string
+    );
+
+
+    return $string;
+}
+foreach ($archivos as $archivo){
+  print $archivo;
+  
+  for ($i = 0; $i<strlen ($archivo); $i++){
+    
+  }
+  
+  print  '--';
+//  $vocales =       array('a','e','i','o','u','n','A','E','I','O','U','N','_');
+//  $vocalesAcento = array('á','é','í','ó','ú','ñ','Á','É','Í','Ó','Ú','Ñ',' ');
+//  $nuevo = str_replace ($vocalesAcento,$vocales,$archivo);
+//  
+//  $nuevo = strtr($archivo,'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
+//                          'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+  print sanear_string($archivo);
+  print  '<br>';
+//  rename ( $archivo, );
+}

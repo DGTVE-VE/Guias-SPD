091197544db8c2109d0b7e80aa5fe1c1f147dcb2
Sonia Martinez <soniamartinezctr@gmail.com>
2015-05-28 15:44:34 -0500

    se copio la carpeta de guías

--- changeNames.php
+++ changeNames.php
@@ -1,85 +1,103 @@
 <?php
 
 $archivos = scandir ( 'guias' );
-function sanear_string($string)
-{
+//function sanear_string($string)
+//{
+//
+//    $string = trim($string);
+//
+//    $string = str_replace(
+//        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+//        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+//        $string
+//    );
+//
+//    $string = str_replace(
+//        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+//        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+//        $string
+//    );
+//
+//    $string = str_replace(
+//        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+//        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+//        $string
+//    );
+//
+//    $string = str_replace(
+//        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+//        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+//        $string
+//    );
+//
+//    $string = str_replace(
+//        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+//        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+//        $string
+//    );
+//
+//    $string = str_replace(
+//        array('ñ', 'Ñ', 'ç', 'Ç'),
+//        array('n', 'N', 'c', 'C',),
+//        $string
+//    );
+//
+//    //Esta parte se encarga de eliminar cualquier caracter extraño
+//    $string = str_replace(
+//        array("\\", "¨", "º", "-", "~",
+//             "#", "@", "|", "!", "\"",
+//             "·", "$", "%", "&", "/",
+//             "(", ")", "?", "'", "¡",
+//             "¿", "[", "^", "`", "]",
+//             "+", "}", "{", "¨", "´",
+//             ">", "< ", ";", ",", ":",
+//             "."),
+//        '',
+//        $string
+//    );
+//    
+//    $string = str_replace(
+//        array(" "),
+//        '_',
+//        $string
+//    );
+//
+//
+//    return $string;
+//}
 
-    $string = trim($string);
 
-    $string = str_replace(
-        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
-        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
-        $string
-    );
-
-    $string = str_replace(
-        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
-        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
-        $string
-    );
-
-    $string = str_replace(
-        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
-        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
-        $string
-    );
-
-    $string = str_replace(
-        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
-        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
-        $string
-    );
-
-    $string = str_replace(
-        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
-        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
-        $string
-    );
-
-    $string = str_replace(
-        array('ñ', 'Ñ', 'ç', 'Ç'),
-        array('n', 'N', 'c', 'C',),
-        $string
-    );
-
-    //Esta parte se encarga de eliminar cualquier caracter extraño
-    $string = str_replace(
-        array("\\", "¨", "º", "-", "~",
-             "#", "@", "|", "!", "\"",
-             "·", "$", "%", "&", "/",
-             "(", ")", "?", "'", "¡",
-             "¿", "[", "^", "`", "]",
-             "+", "}", "{", "¨", "´",
-             ">", "< ", ";", ",", ":",
-             "."),
-        '',
-        $string
-    );
-    
-    $string = str_replace(
-        array(" "),
-        '_',
-        $string
-    );
-
-
-    return $string;
-}
 foreach ($archivos as $archivo){
+  $vocales =       array('a','e','i','o','u','n','','A','E','I','O','U','N');
+  $vocalesAcento = array('á','é','í','ó','ú','ñ','´','Á','É','Í','Ó','Ú','Ñ');    
+   
+//  $lower = strtolower($archivo);
+//  $nuevo = str_replace ('-',' ',$lower);  
+  $nuevo1 = str_replace ($vocalesAcento,$vocales,$archivo);
+  $lower = strtolower($archivo);
+  $nuevo2 = str_replace ($vocalesAcento,$vocales,$lower);
+  $lower2 = strtolower($nuevo2);
+  print '<br>';
   print $archivo;
-  
-  for ($i = 0; $i<strlen ($archivo); $i++){
-    
-  }
-  
-  print  '--';
-//  $vocales =       array('a','e','i','o','u','n','A','E','I','O','U','N','_');
-//  $vocalesAcento = array('á','é','í','ó','ú','ñ','Á','É','Í','Ó','Ú','Ñ',' ');
-//  $nuevo = str_replace ($vocalesAcento,$vocales,$archivo);
-//  
-//  $nuevo = strtr($archivo,'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
-//                          'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
-  print sanear_string($archivo);
-  print  '<br>';
-//  rename ( $archivo, );
+    print '<br>';
+  print $nuevo1;
+  print '<br>';
+  print $lower;
+  print '<br>';
+  print $nuevo2;
+  print '<br>';
+  print $lower2;
 }
+
+
+//foreach ($archivos as $archivo) {
+//    print '<br>';    
+//    print($archivo);
+//    $lower = strtolower($archivo);
+//    print '<br>'; 
+//    print($lower);    
+//    $vocalesAcento = array('Á','É','Í','Ó','Ú','´','Ñ','á','é','í','ó','ú','ñ',' ');    
+//    $nombre = str_replace ($vocalesAcento,'_',$lower);
+//    print '<br>'; 
+//    print($nombre); 
+//}

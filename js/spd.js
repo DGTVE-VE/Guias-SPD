f2aa09914c548b17da0c4c3cf7214f7ec0541aff
Sonia Martinez <soniamartinezctr@gmail.com>
2015-06-02 12:53:10 -0500

    se comento el estilo en el menú para aparezca el menu en el click

--- js/spd.js
+++ js/spd.js
@@ -1,8 +1,19 @@
-$(document).ready(function() {
-    $("#addResourceBtn").click(function(event) {
+$(document).ready(function () {
+    $("#addResourceBtn").click(function (event) {
         event.preventDefault();
         $("#recursos").append('Nombre: <input type="text" name="nombreRecurso[]" class="form-control"><br>');
         $("#recursos").append('PDF: <input type="file" name="archivoRecurso[]" class="form-control"><br>');
     });
-    
+    //Activar los menús en el click
+    $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
+        // Avoid following the href location when clicking
+        event.preventDefault();
+        // Avoid having the menu to close when clicking
+        event.stopPropagation();
+        // If a menu is already open we close it
+        $('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+        // opening the one you clicked on
+        $(this).parent().addClass('open');
+    });
 });
+
